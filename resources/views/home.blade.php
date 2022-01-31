b77c4b89f9252e7eda47c1b24a7eb4445141efee
GiuliaSossai <giulia.sossai@yahoo.it>
2022-01-31 16:53:52 +0100

    layout home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,31 @@
 
 @section('content')
    
-   <main class="container">
+   <!-- <main class="container">
       <h1>home</h1>
 
+   </main> -->
+
+   <main class="gs-bg-dark">
+      <div class="jumbotron"></div>
+
+      <div class="container">
+         <button class="big-btn">current series</button>
+         <div class="main-cards">
+
+            @foreach (config('comics') as $comic)
+            <div class="card-box">
+               <img src="{{$comic['thumb']}}" alt="{{$comic['series']}}">
+                    
+               <h5>{{$comic['series']}}</h5>
+            </div>
+            @endforeach
+
+         </div>  
+      </div>
+
+      <button>load more</button>
+      
    </main>
 
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,20 @@
-<header class="container">
-   <ul class="nav nav-pills mt-3 mb-5">
-      <li class="nav-item">
-         <a class="nav-link {{ (Route::currentRouteName() === 'home') ? 'active' : '' }}" href="{{ route('home') }}">Home</a>
-      </li>
-      <li class="nav-item">
-         <a class="nav-link {{ (Route::currentRouteName() === 'comics.index') ? 'active' : '' }}" href="{{ route('comics.index') }}">Comics</a>
-      </li>
-      <li class="nav-item">
-         <a class="nav-link {{ (Route::currentRouteName() === 'comics.create') ? 'active' : '' }}" href="{{ route('comics.create') }}">New Comic</a>
-      </li>
-   </ul>
+<header class="container d-flex justify-content-between align-items-center">
+   <div class="header-left">
+      <a href="#"><img src="{{ asset('img/dc-logo.png')}}" alt="dc logo"></a> 
+   </div>
+
+   <div class="header-right">
+      <ul class="nav nav-pills">
+         <li class="nav-item">
+            <a class="nav-link {{ (Route::currentRouteName() === 'home') ? 'active' : '' }}" href="{{ route('home') }}">Home</a>
+         </li>
+         <li class="nav-item">
+            <a class="nav-link {{ (Route::currentRouteName() === 'comics.index') ? 'active' : '' }}" href="{{ route('comics.index') }}">Comics</a>
+         </li>
+         <li class="nav-item">
+            <a class="nav-link {{ (Route::currentRouteName() === 'comics.create') ? 'active' : '' }}" href="{{ route('comics.create') }}">New Comic</a>
+         </li>
+      </ul>
+   </div>
+   
 </header>
